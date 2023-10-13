1727acd297ff17bd832e357e6c73fbaaac43e5a8
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-13 11:58:16 +0200

    Add project files.

---
+++ Check if nmbers are polindrom/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_if_nmbers_are_polindrom
+{
+    internal class Program
+    {
+        static bool IsPalindrome(int number)
+        {
+            if (number < 0)
+            {
+                return false;
+            }
+            int reversedNumber = 0;
+            int originalNumber = number;
+            while (originalNumber > 0)
+            {
+                int lastDigit = originalNumber % 10;
+                reversedNumber = reversedNumber * 10 + lastDigit;
+                originalNumber /= 10;
+            }
+            return number == reversedNumber;
+
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if it is a palindrome:");
+            int number = int.Parse(Console.ReadLine());
+            bool isPalindrome = IsPalindrome(number);
+            if (isPalindrome == true)
+            {
+                Console.WriteLine("The number {0} is a palindrome.", number);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is not a palindrome.", number);
+            }
+        }   
+    }
+}
